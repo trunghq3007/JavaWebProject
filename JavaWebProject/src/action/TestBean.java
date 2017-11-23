a42711e008fc0138997e0c26952f1e83aa6b0382
Admin <Admin@10.168.10.84>
2017-11-23 09:14:14 +0700

    jsp servlet commit

--- JavaWebProject/src/action/TestBean.java
+++ JavaWebProject/src/action/TestBean.java
@@ -1,36 +1,12 @@
 package action;
 
-/**
- * @description:
- * @author: Admin
- * @time: 5:46:25 PM
- * @date: Nov 22, 2017
- */
 public class TestBean {
 	private String message = "No message specified";
 
-	 /**
-	 * @description: 
-	 * @author:Admin CMC Corporation
-	 * @create date:Nov 22, 2017
-	 * @modifier:Admin
-	 * @modifined date: Nov 22, 2017
-	 * @exception:
-	 * @return
-	 */
 	public String getMessage() {
 		return (message);
 	}
 
-	 /**
-	 * @description: 
-	 * @author:Admin CMC Corporation
-	 * @create date:Nov 22, 2017
-	 * @modifier:Admin
-	 * @modifined date: Nov 22, 2017
-	 * @exception:
-	 * @param message
-	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
